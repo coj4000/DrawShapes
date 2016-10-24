e8e70f9c6b7c41063fbe31d4aa8561f3f65af097
Christian <chri9j83@edu.easj.dk>
2016-10-24 11:08:48 +0200

    Done

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -12,9 +12,53 @@ namespace Sandbox
 
 
             // YOUR JOB: Add code to draw shapes
-
-
-            // The LAST line of code should be ABOVE this line
-        }
+            for (int i = 0; i < 10; i++)
+            {
+                drawer.DrawOneStar();
+            }
+            drawer.StartNewLine();
+            for (int i = 0; i < 5; i++)
+            {
+                drawer.DrawOneStar();
+                drawer.DrawOneSpace();
+            }
+            drawer.StartNewLine();
+            drawer.StartNewLine();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    drawer.DrawOneStar();
+                }
+                
+                drawer.StartNewLine();
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                int e = 0;
+                for (int x = 0; x < i; x++)
+                {
+                    drawer.DrawOneStar();
+                }
+               
+                drawer.StartNewLine();
+            }
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    if ((x == y) || (x + y == 9))
+                    {
+                        drawer.DrawOneStar();
+                    }
+                    else
+                    {
+                        drawer.DrawOneSpace();
+                    }
+                }
+                drawer.StartNewLine();
+            }
+        // The LAST line of code should be ABOVE this line
+    }
     }
 }
